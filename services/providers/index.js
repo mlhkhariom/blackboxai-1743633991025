45451c6d73bbf24ae25a6ce2cf5e455efd2d1d6d
Hariom Vishwkarma <62866892+mlhkhariom@users.noreply.github.com>
2025-04-03 04:16:37 +0530

    Add services/providers/index.js 1743633995921

---
+++ services/providers/index.js
@@ -0,0 +1,23 @@
+import bollyflix from './bollyflix';
+import netflixmirror from './netflixmirror';
+import vegamovies from './vegamovies';
+import moviesdrive from './moviesdrive';
+import moviesmod from './moviesmod';
+
+// Export all providers
+export default {
+  bollyflix,
+  netflixmirror,
+  vegamovies,
+  moviesdrive,
+  moviesmod
+};
+
+// Export individual providers
+export {
+  bollyflix,
+  netflixmirror,
+  vegamovies,
+  moviesdrive,
+  moviesmod
+};
